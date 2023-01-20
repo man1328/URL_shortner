6a35cbc6d3259d439f04d494ee7f2d8ced5d3e2f
man1328 <83740663+man1328@users.noreply.github.com>
2023-01-19 22:20:38 -0500

    Update ShortURL.py

--- ShortURL.py
+++ ShortURL.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 
 
-driver = webdriver.Firefox(service=Service(executable_path=GeckoDriverManager().install()))
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
 driver.get("https://www.shorturl.at/")
 driver.implicitly_wait(3)
 search = driver.find_element(By.CSS_SELECTOR, "input[name='u']")
